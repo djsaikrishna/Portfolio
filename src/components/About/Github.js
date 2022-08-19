882ed477247a224621c1a1ea3df3f6b1d9d979e1
Vinayak Mundakkal <playstorepointsusa@gmail.com>
2022-08-19 11:26:23 +0530

    github

--- src/components/About/Github.js
+++ src/components/About/Github.js
@@ -9,7 +9,7 @@ function Github() {
         Days I <strong className="purple">Code</strong>
       </h1>
       <GitHubCalendar
-        username="soumyajit4419"
+        username="vinayak-7-0-3"
         blockSize={15}
         blockMargin={5}
         color="#c084f5"
